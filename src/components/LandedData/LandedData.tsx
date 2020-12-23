277b9978492ce315fe3449a9dcbe42228f01f4e9
Marczeli <aleczas1@onet.pl>
2020-12-23 02:14:17 +0100

    Optimized LandedData

--- src/components/LandedData/LandedData.tsx
+++ src/components/LandedData/LandedData.tsx
@@ -1,15 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useGetdata } from '../../utils/useGetdata';
 import DisplayData from '../Shared/DisplayData';
 import DisplayMap from '../Shared/DisplayMap';
 
 const LandedData: React.FC<{ landedUserIP: string }> = ({ landedUserIP }) => {
-  const [getData, data] = useGetdata(landedUserIP, true);
+  const [, data] = useGetdata(landedUserIP, true);
 
-  useEffect(() => {
-    getData();
-  }, [getData]);
-  
   return (
     <div className='landedData__container'>
       <DisplayMap location={data} top={true} />
